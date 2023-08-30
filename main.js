4369f79ccec5b8d86d5667001c9d54d03f0f26d8
yagoluucas <yago543@gmail.com>
2023-08-29 23:11:10 -0300

    Adicionado a seta para voltar e as seções

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const next = document.querySelectorAll("[data-next]")
+const previous = document.querySelectorAll('[data-previous]')
 const questions = document.querySelectorAll('section')
 
 next.forEach(next => {
-    next.addEventListener('click', proximoElemento)
+    next.addEventListener('click', nextElement)
 
-    function proximoElemento(event){
+    function nextElement(event){
         questions.forEach(secao => {
 
            if(event.target.parentNode.classList.contains('ativo')) {
@@ -12,6 +13,19 @@ next.forEach(next => {
                 event.target.parentNode.nextElementSibling.classList.add('ativo')
            }
 
+        })
+    }
+})
+
+previous.forEach(previous => {
+    previous.addEventListener('click', previousElement)
+
+    function previousElement(event) {
+        questions.forEach(secao => {
+            if(event.target.parentNode.classList.contains('ativo')) {
+                event.target.parentNode.classList.remove('ativo')
+                event.target.parentNode.previousElementSibling.classList.add('ativo')
+            }
         })
     }
 })
